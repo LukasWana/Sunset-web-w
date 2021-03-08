d0efbb38d85a2a9725d25faf3d34b320d281529d
lukas-wana <lukas-wana@users.noreply.github.com>
2021-03-08 08:51:31 +0100

    pridano - Sledujte nas TW & FB

--- src/pages/IndexJidelna.vue
+++ src/pages/IndexJidelna.vue
@@ -1,3 +1,111 @@
+<script>
+import AnimatedNumber from 'components/AnimatedNumber'
+import CarouselContent from 'components/CarouselContent'
+import PageSectionDoubleCard from 'components/PageSectionDoubleCard'
+import ReferenceCard from 'components/ReferenceCard'
+import JidelnaGallery from 'components/JidelnaGallery'
+import JidelnaSectionDoubleCard from 'components/JidelnaSectionDoubleCard'
+import JidelnaSectionPriceCard from 'components/JidelnaSectionPriceCard'
+import JidelnaSectionBlogCard from 'components/JidelnaSectionBlogCard'
+import JidelnaFormular from '../components/Formular'
+import PageSection from 'components/PageSection'
+import PageSectionCard from 'components/PageSectionCard'
+import LayoutFooter from 'layouts/LayoutFooter'
+import { scroll, date } from 'quasar'
+import axios from 'axios'
+
+const { setScrollPosition, getScrollTarget } = scroll
+
+// pokus o pocesteni datumu
+// https://quasar.dev/quasar-utils/date-utils#Manipulate-dates
+// const obj = date.extractDate({
+// months: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec']
+// })
+
+
+export default {
+  name: 'PageIndex',
+
+  meta: {
+    title: 'JidelnaSQL'
+  },
+
+  components: {
+    JidelnaGallery,
+    AnimatedNumber,
+    JidelnaSectionDoubleCard,
+    JidelnaSectionPriceCard,
+    JidelnaSectionBlogCard,
+    ReferenceCard,
+    CarouselContent,
+    PageSectionDoubleCard,
+    JidelnaFormular,
+    LayoutFooter,
+    PageSectionCard,
+    PageSection
+  },
+
+  async beforeMount () {
+    const result = await axios.get('https://blog.altisima.cz/ghost/api/v3/content/posts/?key=48078e4106f7bfb20482486b1e&filter=tag:jidelna-sql&limit=3&order=published_at%20desc&format=html,text')
+    this.blogPosts = result.data.posts
+  },
+
+  data () {
+    return {
+      slide: '1',
+      slideReferences: '1',
+      tab: 'internetoveobj',
+      imagesInternet: [
+        { src: 'statics/galerie/int-obj/1_objednavka.png', alt: 'Objednávka', thumbnail: null },
+        { src: 'statics/galerie/int-obj/2_prehled.PNG', alt: 'Přehled', thumbnail: null },
+        { src: 'statics/galerie/int-obj/3_finance.png', alt: 'Finance', thumbnail: null },
+        { src: 'statics/galerie/int-obj/4_maily.png', alt: 'Maily', thumbnail: null }
+      ],
+      imagesJidelna: [
+        { src: 'statics/galerie/jid-sql/1_titulni-orbazovka.jpg', alt: 'Titulní obrazovka', thumbnail: null },
+        { src: 'statics/galerie/jid-sql/2_jidelnicek.jpg', alt: 'Jídelníček', thumbnail: null },
+        { src: 'statics/galerie/jid-sql/3_pokladna.jpg', alt: 'Pokladna', thumbnail: null }
+      ],
+      imagesObjednavkovy: [
+        { src: 'statics/galerie/obj-ter/1_TT1703.jpg', alt: 'Terminál', thumbnail: null },
+        { src: 'statics/galerie/obj-ter/2_TT1703.jpg', alt: 'Terminál', thumbnail: null },
+        { src: 'statics/galerie/obj-ter/3_identifikace.jpg', alt: 'Identifikace', thumbnail: null },
+        { src: 'statics/galerie/obj-ter/4_objednavka.jpg', alt: 'Objednávka', thumbnail: null }
+      ],
+      imagesVydej: [
+        { src: 'statics/galerie/vyd-ter/1_TT3503vydej.jpg', alt: 'Terminál výdej', thumbnail: null },
+        { src: 'statics/galerie/vyd-ter/2_identifikace.jpg', alt: 'Identifikace', thumbnail: null },
+        { src: 'statics/galerie/vyd-ter/3_vydej.jpg', alt: 'Výdej', thumbnail: null },
+        { src: 'statics/galerie/vyd-ter/4_TT3511semafor.jpg', alt: 'Semafor na terminálu', thumbnail: null }
+      ],
+      blogPosts: []
+    }
+  },
+
+  methods: {
+    login () {
+      window.location.href = 'https://jidelna-sql.altisima.cz'
+    },
+
+    scrollToObjednat () {
+      // console.log('objednat clicked')
+      // console.log({ 'this.$refs': this.$refs })
+      const el = this.$refs.formular.$el
+      // console.log({ 'el': el })
+      const target = getScrollTarget(el)
+      const offset = el.offsetTop
+      const duration = 600
+      setScrollPosition(target, offset, duration)
+    },
+
+    formatDate (datum) {
+      return date.formatDate(datum, 'DD MMMM, YYYY')
+    }
+  }
+
+}
+</script>
+
 <template lang="pug">
   q-page
     page-section(:fullWidth="true")
@@ -344,9 +452,16 @@
 
     page-section(ref="formular")
       .row.justify-center.page-section-ourstory
-        .col-xs-12.col-sm-8.col-md-6.q-px-md.q-mb-xl
+        .col-xs-12.col-sm-8.col-md-6.q-px-md
           h2.text-center Kontaktujte nás
           jidelna-formular
+
+    page-section.section-nadpis.text-center
+      .row.justify-center.page-section-ourstory
+        .col-xs-12.col-sm-8.col-md-7.q-px-md.q-mb-xl
+          h4.q-mb-md Sledujte nás
+          q-btn(round outline color='primary', icon='eva-facebook', type="a" href="https://www.facebook.com/altisimacz" target="_blank" size="15px")
+          q-btn.q-mx-lg(round outline color='primary' icon='eva-twitter' type="a" href="https://twitter.com/AltisimaSe?ref_src=twsrc%5Etfw" target="_blank" size="15px")
 
 </template>
 
@@ -439,110 +554,3 @@ ul
 
 </style>
 
-<script>
-import AnimatedNumber from 'components/AnimatedNumber'
-import CarouselContent from 'components/CarouselContent'
-import PageSectionDoubleCard from 'components/PageSectionDoubleCard'
-import ReferenceCard from 'components/ReferenceCard'
-import JidelnaGallery from 'components/JidelnaGallery'
-import JidelnaSectionDoubleCard from 'components/JidelnaSectionDoubleCard'
-import JidelnaSectionPriceCard from 'components/JidelnaSectionPriceCard'
-import JidelnaSectionBlogCard from 'components/JidelnaSectionBlogCard'
-import JidelnaFormular from '../components/Formular'
-import PageSection from 'components/PageSection'
-import PageSectionCard from 'components/PageSectionCard'
-import LayoutFooter from 'layouts/LayoutFooter'
-import { scroll, date } from 'quasar'
-import axios from 'axios'
-
-const { setScrollPosition, getScrollTarget } = scroll
-
-// pokus o pocesteni datumu
-// https://quasar.dev/quasar-utils/date-utils#Manipulate-dates
-// const obj = date.extractDate({
-// months: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec']
-// })
-
-
-export default {
-  name: 'PageIndex',
-
-  meta: {
-    title: 'JidelnaSQL'
-  },
-
-  components: {
-    JidelnaGallery,
-    AnimatedNumber,
-    JidelnaSectionDoubleCard,
-    JidelnaSectionPriceCard,
-    JidelnaSectionBlogCard,
-    ReferenceCard,
-    CarouselContent,
-    PageSectionDoubleCard,
-    JidelnaFormular,
-    LayoutFooter,
-    PageSectionCard,
-    PageSection
-  },
-
-  async beforeMount () {
-    const result = await axios.get('https://blog.altisima.cz/ghost/api/v3/content/posts/?key=48078e4106f7bfb20482486b1e&filter=tag:jidelna-sql&limit=3&order=published_at%20desc&format=html,text')
-    this.blogPosts = result.data.posts
-  },
-
-  data () {
-    return {
-      slide: '1',
-      slideReferences: '1',
-      tab: 'internetoveobj',
-      imagesInternet: [
-        { src: 'statics/galerie/int-obj/1_objednavka.png', alt: 'Objednávka', thumbnail: null },
-        { src: 'statics/galerie/int-obj/2_prehled.PNG', alt: 'Přehled', thumbnail: null },
-        { src: 'statics/galerie/int-obj/3_finance.png', alt: 'Finance', thumbnail: null },
-        { src: 'statics/galerie/int-obj/4_maily.png', alt: 'Maily', thumbnail: null }
-      ],
-      imagesJidelna: [
-        { src: 'statics/galerie/jid-sql/1_titulni-orbazovka.jpg', alt: 'Titulní obrazovka', thumbnail: null },
-        { src: 'statics/galerie/jid-sql/2_jidelnicek.jpg', alt: 'Jídelníček', thumbnail: null },
-        { src: 'statics/galerie/jid-sql/3_pokladna.jpg', alt: 'Pokladna', thumbnail: null }
-      ],
-      imagesObjednavkovy: [
-        { src: 'statics/galerie/obj-ter/1_TT1703.jpg', alt: 'Terminál', thumbnail: null },
-        { src: 'statics/galerie/obj-ter/2_TT1703.jpg', alt: 'Terminál', thumbnail: null },
-        { src: 'statics/galerie/obj-ter/3_identifikace.jpg', alt: 'Identifikace', thumbnail: null },
-        { src: 'statics/galerie/obj-ter/4_objednavka.jpg', alt: 'Objednávka', thumbnail: null }
-      ],
-      imagesVydej: [
-        { src: 'statics/galerie/vyd-ter/1_TT3503vydej.jpg', alt: 'Terminál výdej', thumbnail: null },
-        { src: 'statics/galerie/vyd-ter/2_identifikace.jpg', alt: 'Identifikace', thumbnail: null },
-        { src: 'statics/galerie/vyd-ter/3_vydej.jpg', alt: 'Výdej', thumbnail: null },
-        { src: 'statics/galerie/vyd-ter/4_TT3511semafor.jpg', alt: 'Semafor na terminálu', thumbnail: null }
-      ],
-      blogPosts: []
-    }
-  },
-
-  methods: {
-    login () {
-      window.location.href = 'https://jidelna-sql.altisima.cz'
-    },
-
-    scrollToObjednat () {
-      // console.log('objednat clicked')
-      // console.log({ 'this.$refs': this.$refs })
-      const el = this.$refs.formular.$el
-      // console.log({ 'el': el })
-      const target = getScrollTarget(el)
-      const offset = el.offsetTop
-      const duration = 600
-      setScrollPosition(target, offset, duration)
-    },
-
-    formatDate (datum) {
-      return date.formatDate(datum, 'DD MMMM, YYYY')
-    }
-  }
-
-}
-</script>
