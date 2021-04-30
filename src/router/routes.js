0b5c5f7f0a74de7a73a9b4654fba3d9e962b9b5f
lukas-wana <lukas-wana@users.noreply.github.com>
2021-04-30 13:58:12 +0200

    prepare

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,12 @@ const routes = [
     props: true,
     component: () => import('src/layouts/Layout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexJidelna') }
+      { path: '', component: () => import('pages/IndexJidelna') },
+      { name: 'welcome', path: 'welcome', component: () => import('pages/Public/WelcomePage.vue') },
+      { name: 'about', path: 'about', component: () => import('pages/Public/AboutPage.vue') },
+      { name: 'photo', path: 'photo', component: () => import('pages/Public/PhotoPage.vue') },
+      { name: 'usefull', path: 'usefull', component: () => import('pages/Public/UsefullPage.vue') },
+      { name: 'contact', path: 'contact', component: () => import('pages/Public/ContactPage.vue') }
     ]
   }
 
