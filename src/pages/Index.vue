bf50a329c51957856d1d6af517f6e8cca8354b1d
lukas-wana <lukas-wana@users.noreply.github.com>
2021-05-13 08:12:13 +0200

    pdf

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -199,7 +199,7 @@ export default {
       .row
         .col-xs-12.col-sm-1.col-md-2.col-lg-3.q-mr-sm
         .col-xs-12.col-sm-9.col-md-7.col-lg-6
-          a(href="https://sunset-apartment.com/download/Uzitecne-rady-a-tipy.pdf" target="_blank").invisible-link
+          a(href="statics/Uzitecne-rady-a-tipy.pdf" target="_blank").invisible-link
             .row.items-center
               .col-1.q-mr-sm.offset-2
                 img(src="~assets/pdf-icon-color.svg" height="36px")
